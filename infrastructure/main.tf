579e611dbc3d8fe7f5198d020eb879be5db7a946
maharajan-jegan <maharajan.jegan@hmcts.net>
2021-10-07 10:48:39 +0100

    infra fix

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -19,6 +19,28 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
+
+module "ccpay-vault" {
+  source = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+  name = join("-", [var.product, var.env])
+  product = var.product
+  env = var.env
+  tenant_id = var.tenant_id
+  object_id = var.jenkins_AAD_objectId
+  resource_group_name = azurerm_resource_group.rg.name
+  # group id of dcd_reform_dev_azure
+  product_group_name = "dcd_group_fees&pay_v2"
+  common_tags         = var.common_tags
+  #aks migration
+  managed_identity_object_ids = ["${var.managed_identity_object_id}"]
+  create_managed_identity = true
+}
+
+data "azurerm_key_vault" "ccpay_key_vault" {
+  name                = module.ccpay-vault.key_vault_name
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
 module "servicebus-namespace" {
   source              = "git@github.com:hmcts/terraform-module-servicebus-namespace"
   name                = "${var.product}-servicebus-${var.env}"
