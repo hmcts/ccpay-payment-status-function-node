20b2bcf1ef709f8678124fe52a94ea166d87033b
Vijay Kumar Rajagopalan <62386423+vijayrajagopalan-hmcts@users.noreply.github.com>
2021-10-12 17:57:39 +0100

    Pay 5227 changes (#17)
    * resource group name added
    
    * tags fix on infra
    
    * feature added in infra mainf.tf
    
    * fix infra
    
    * infra fix
    
    * cpo fix unwanted
    
    * demo.tfvars
    
    * ansi-regx fix
    
    * regx fix
    
    * removed managed identity
    
    * demo env added
    
    * RG removed
    
    * fix for rg
    
    * RG changed
    
    * RG change
    
    * Replace module blocks with data blocks.
    
    Co-authored-by: maharajan-jegan <maharajan.jegan@hmcts.net>
    Co-authored-by: maharajan-jegan <83658764+maharajan-jegan@users.noreply.github.com>

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,30 +1,47 @@
-locals {
-  subscription_name = "defaultServiceCallbackSubscription"
-  retry_queue = "serviceCallbackRetryQueue"
+provider "azurerm" {
+  features {}
 }
 
-module "servicebus-namespace" {
-  source              = "git@github.com:hmcts/terraform-module-servicebus-namespace"
-  name                = "${var.product}-servicebus-${var.env}"
-  location            = var.location
-  env                 = var.env
-  common_tags         = local.tags
-  resource_group_name = azurerm_resource_group.rg.name
+locals {
+  subscription_name = "defaultServiceCallbackSubscription"
+  
+  # retry_queue = "serviceCallbackRetryQueue"
+  # local_tags_ccpay = {
+  #   "Deployment Environment" = var.env
+  #   "Team Name" = var.team_name
+  #   "Team Contact" = var.team_contact
+  # }
+
+  # tags = merge(var.common_tags, local.local_tags_ccpay)
+}
+
+data "azurerm_resource_group" "ccpay_rg" {
+  name     = join("-", [var.product, var.env])
+}
+
+data "azurerm_key_vault" "ccpay_key_vault" {
+  name                = join("-", [var.product, var.env])
+  resource_group_name = data.azurerm_resource_group.ccpay_rg.name
+}
+
+data "azurerm_servicebus_namespace" "ccpay_servicebus_namespce" {
+  name                = join("-", [var.product, "servicebus", var.env])
+  resource_group_name = data.azurerm_resource_group.ccpay_rg.name
 }
 
 module "topic_payment_status" {
-  source                = "git@github.com:hmcts/terraform-module-servicebus-topic"
+  source                = "git@github.com:hmcts/terraform-module-servicebus-topic?ref=master"
   name                  = "ccpay-payment-status-topic"
-  namespace_name        = module.servicebus-namespace.name
-  resource_group_name   = azurerm_resource_group.rg.name
+  namespace_name        = data.azurerm_servicebus_namespace.ccpay_servicebus_namespce.name
+  resource_group_name   = data.azurerm_resource_group.ccpay_rg.name
 }
 
 module "subscription_payment_status" {
-  source                = "git@github.com:hmcts/terraform-module-servicebus-subscription"
+  source                = "git@github.com:hmcts/terraform-module-servicebus-subscription?ref=master"
   name                  = local.subscription_name
-  namespace_name        = module.servicebus-namespace.name
+  namespace_name        = data.azurerm_servicebus_namespace.ccpay_servicebus_namespce.name
   topic_name            = module.topic_payment_status.name
-  resource_group_name   = azurerm_resource_group.rg.name
+  resource_group_name   = data.azurerm_resource_group.ccpay_rg.name
   max_delivery_count    = "10"
   # forward_dead_lettered_messages_to = module.queue.name
 }
@@ -34,4 +51,3 @@ resource "azurerm_key_vault_secret" "ccpay-payment-status-connection-string" {
   value        = module.topic_payment_status.primary_send_and_listen_shared_access_key
   key_vault_id = data.azurerm_key_vault.ccpay_key_vault.id
 }
-
