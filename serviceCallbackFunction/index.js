7fbf83190015e60d2cbcc049eeaf4044d465cb30
Jennings-HMCTS <jennings.mcenroe@HMCTS.NET>
2021-10-19 14:14:11 +0100

    changed to payment-callback-function in index.js jenkins files and sonar prop files

--- serviceCallbackFunction/index.js
+++ serviceCallbackFunction/index.js
@@ -7,7 +7,7 @@ appInsights.setup(config.get('appInsightsInstumentationKey'))
     .setDistributedTracingMode(appInsights.DistributedTracingModes.AI_AND_W3C)
     .setSendLiveMetrics(true)
     .start();
-appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = 'ccpay-callback-function';
+appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = 'ccpay-payment-callback-function';
 appInsights.defaultClient.config.maxBatchSize = 0;
 serviceCallbackFunction().catch((err) => {
   console.log("Error occurred: ", err);
